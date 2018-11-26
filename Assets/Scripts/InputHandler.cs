c4e33a0197a8d535aa5aaf90d0d3721d64085b84
moecia <leebingnam@gmail.com>
2018-11-26 13:55:33 -0800

    New build

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -12,7 +12,8 @@ public class InputHandler : MonoBehaviour {
 
     private bool startMove = false;
     [SerializeField] private float moveSpeed = 1.0f;
-    [SerializeField] private Transform moverReference;
+    [SerializeField] private Transform[] moverReference = new Transform[2];
+    [SerializeField] private int currentPlayer = 0;
 
     [SerializeField] private GameObject footStep_L;
     [SerializeField] private GameObject footStep_R;
@@ -22,22 +23,19 @@ public class InputHandler : MonoBehaviour {
 
     private List<GameObject> footStepCache = new List<GameObject>();
 
-    [SerializeField] public Animator footstepMarker;
+    [SerializeField] public Animator[] footstepMarker = new Animator[2];
 
     [SerializeField] private Vector3 testPos;
 
     // Use this for initialization
     void Start () 
     {
-        lastPos = moverReference.position;
+        ActivatePlayer(0);
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        // print("Mouse Pos: " + Input.mousePosition);
-        // print("Convert to World Pos: " + Camera.main.ScreenToWorldPoint(Input.mousePosition));
-
         if (startMove)
             GenerateFootstep();
     }
@@ -48,47 +46,19 @@ public class InputHandler : MonoBehaviour {
         startMove = true;
     }
 
-    private void GenerateFootstep()
+    public void ActivatePlayer(int index)
     {
-        if (Mathf.Abs(moverReference.position.x - newPos.x) <= .1f
-         && Mathf.Abs(moverReference.position.y - newPos.y) <= .1f)
+        currentPlayer = index;
+        startMove = false;
+        if (index == 0)
         {
-            footstepMarker.SetTrigger("Appear");
-            print("Arrival: " + System.DateTime.Now.ToString());
-            lastPos = newPos;
-            startMove = false;
+            moverReference[0].gameObject.SetActive(true);
+            moverReference[1].gameObject.SetActive(false);
         }
-        else
+        else if (index == 1)
         {
-            moverReference.transform.position += moveSpeed * direction * Time.deltaTime;
-            if (timer < generateInteval)
-                timer += Time.deltaTime;
-            else
-            {
-                var dir = newPos - lastPos;
-                float angle = -Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
-                Quaternion newRot = Quaternion.AngleAxis(angle, Vector3.forward);
-
-                footstepMarker.transform.rotation = newRot;
-
-                if (leftOrRight == 0)
-                {
-                    GameObject temp = Instantiate(footStep_L,
-                        moverReference.transform.position,
-                        newRot);
-                    footStepCache.Add(temp);
-                    leftOrRight = 1;
-                }
-                else
-                {
-                    GameObject temp = Instantiate(footStep_R,
-                        moverReference.transform.position,
-                        newRot);
-                    footStepCache.Add(temp);
-                    leftOrRight = 0;
-                }
-                timer = 0;
-            }
+            moverReference[1].gameObject.SetActive(true);
+            moverReference[0].gameObject.SetActive(false);
         }
     }
 
@@ -124,6 +94,7 @@ public class InputHandler : MonoBehaviour {
             footStepCache.Clear();
         }
 
+        lastPos = moverReference[currentPlayer].position;
         if (useViewportPos)
             newPos = Camera.main.ScreenToWorldPoint(new Vector3(posX, posY));            
         newPos.z = 0;
@@ -132,11 +103,54 @@ public class InputHandler : MonoBehaviour {
         timer = generateInteval;
 
         if (!startMove)
-            footstepMarker.SetTrigger("Disappear");
+            footstepMarker[currentPlayer].SetTrigger("Disappear");
 
         StartCoroutine(DelayStart());
     }
 
+    private void GenerateFootstep()
+    {
+        if (Mathf.Abs(moverReference[currentPlayer].position.x - newPos.x) <= .1f
+         && Mathf.Abs(moverReference[currentPlayer].position.y - newPos.y) <= .1f)
+        {
+            footstepMarker[currentPlayer].SetTrigger("Appear");
+            print("Arrival: " + System.DateTime.Now.ToString());
+            lastPos = newPos;
+            startMove = false;
+        }
+        else
+        {
+            moverReference[currentPlayer].transform.position += moveSpeed * direction * Time.deltaTime;
+            if (timer < generateInteval)
+                timer += Time.deltaTime;
+            else
+            {
+                var dir = newPos - lastPos;
+                float angle = -Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+                Quaternion newRot = Quaternion.AngleAxis(angle, Vector3.forward);
+
+                footstepMarker[currentPlayer].transform.rotation = newRot;
+
+                if (leftOrRight == 0)
+                {
+                    GameObject temp = Instantiate(footStep_L,
+                        moverReference[currentPlayer].transform.position,
+                        newRot);
+                    footStepCache.Add(temp);
+                    leftOrRight = 1;
+                }
+                else
+                {
+                    GameObject temp = Instantiate(footStep_R,
+                        moverReference[currentPlayer].transform.position,
+                        newRot);
+                    footStepCache.Add(temp);
+                    leftOrRight = 0;
+                }
+                timer = 0;
+            }
+        }
+    }
 
     // Deleted the following in build ver.
     //[CustomEditor(typeof(InputHandler))]
